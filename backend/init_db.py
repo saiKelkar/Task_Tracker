64fedadac016b67639d0e2b97b017cc458803581
saiKelkar <kelkars1999@gmail.com>
2025-08-22 21:29:04 +0530

    Move DB initialization to main.py and remove init_db.py

--- backend/init_db.py
+++
@@ -1,5 +0,0 @@
-from database import Base, engine
-from models import Tasks
-
-Base.metadata.create_all(bind=engine)
-print("Tables creates")
--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-
+from database import Base, engine
+import models
 from routes import task_routes
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
